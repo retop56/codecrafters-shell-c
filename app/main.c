418c5648402364b4f355f62e04af0627b12a9e08
Big Homie KG <retop56@gmail.com>
2024-12-02 20:48:29 -0800

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   // Uncomment this block to pass the first stage
@@ -8,5 +9,8 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  int len_of_input = strlen(input);
+  input[len_of_input - 1] = '\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
