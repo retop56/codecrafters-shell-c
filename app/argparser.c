ae9e64155f267fb0351ee9983e4a8b8166422001
Big Homie KG <retop56@gmail.com>
2025-12-19 01:00:21 -0800

    Mid-stage #NI6

--- app/argparser.c
+++ app/argparser.c
@@ -1,5 +1,7 @@
 #include "cc_shell.h"
 #include "arg_obj_def.h"
+#include <stdlib.h>
+#include <string.h>
 #include "argparser.h"
 
 struct arg_obj * create_arg_obj() {
@@ -15,7 +17,7 @@ struct arg_obj * create_arg_obj() {
   return ao;
 };
 
-void expand_arg_obj(struct arg_obj *ao) {
+void expand_arg_obj(struct arg_obj * ao) {
   ao->limit = ao->limit * 2;
   char ** new_args = realloc(ao->args, ao->limit * 2);
   if (new_args == NULL) {
@@ -24,31 +26,67 @@ void expand_arg_obj(struct arg_obj *ao) {
   }
 }
 
-void add_args(struct arg_obj *ao, char *input) {
-  char * tok;
-  // 1. Call strtok on input
-  tok = strtok(input, " ");
-  char * new_arg;
-  // 2. Create loop that will continue to run as long as tok is not null AKA there's another
-  //    token left
-  while (tok != NULL) {
-    // 3. If it's successful, first check if there's enough space for this argument.
-    //    If there isn't, expand arg_obj first
+void add_args(struct arg_obj * ao, char * input) {
+  if (input == NULL || ao == NULL) {
+    return;
+  }
+  while (*input != '\0') {
+    /*
+     * Check to see if arg_obj has enough space for next arg. If it doesn't,
+     * expand the size of arg_obj->args array
+     */
     if ((ao->size + 1) >= ao->limit) {
       expand_arg_obj(ao);
     }
-    new_arg = strdup(tok);
-    if (new_arg == NULL) {
-      fprintf(stderr, "Failed to duplicate token before adding to arg_obj!");
+    if (strncmp(input, "\'", 1) == 0) {
+      input = get_single_quote_arg(ao, input);
+    } else if (strncmp(input, "\"", 1) == 0) {
+      printf("Found argument that starts with a double quote!\n");
       exit(EXIT_FAILURE);
+    } else {
+      input = get_normal_arg(ao, input);
     }
-    // 4. Add new_arg to arg_obj array
-    ao->args[ao->size++] = new_arg;
-    tok = strtok(NULL, " "); // Get next token
   }
 }
 
-void clear_args(struct arg_obj *ao) {
+static char * get_normal_arg(struct arg_obj * ao, char * input) {
+  size_t count = 0;
+  char * curr_char = input;
+  while (*curr_char != '\0' && *curr_char != ' ') {
+    count++;
+    curr_char++;
+  }
+  while (*curr_char == ' ') {
+    curr_char++;
+  }
+  char * new_arg = strndup(input, count);
+  if (new_arg == NULL) {
+    fprintf(stderr, "strndup failed at line %d in %s\n", (__LINE__) - 2, __FUNCTION__);
+  }
+  ao->args[ao->size++] = new_arg;
+  return curr_char;
+}
+
+static char * get_single_quote_arg(struct arg_obj * ao, char * input) {
+  input++; // Skip past first single quote
+  size_t count = 0;
+  char * curr_char = input;
+  while (*curr_char != '\0' && *curr_char != '\'') {
+    count++;
+    curr_char++;
+  }
+  if (*curr_char == '\'') {
+    curr_char++;
+  }
+  char * new_arg = strndup(input, count);
+  if (new_arg == NULL) {
+    fprintf(stderr, "strndup failed at line %d in %s\n", (__LINE__) - 2, __FUNCTION__);
+  }
+  ao->args[ao->size++] = new_arg;
+  return curr_char;
+}
+
+void clear_args(struct arg_obj * ao) {
   for (size_t i = 0; i < ao->size; i++) {
     free(ao->args[i]);
   }
--- app/argparser.h
+++ app/argparser.h
@@ -1,5 +1,7 @@
 #include "arg_obj_def.h"
 
-struct arg_obj *create_arg_obj();
-void add_args(struct arg_obj *ao, char *input);
-void clear_args(struct arg_obj *ao);
+struct arg_obj * create_arg_obj();
+void add_args(struct arg_obj * ao, char * input);
+void clear_args(struct arg_obj * ao);
+static char * get_normal_arg(struct arg_obj * ao, char * input);
+static char * get_single_quote_arg(struct arg_obj * ao, char * input);
